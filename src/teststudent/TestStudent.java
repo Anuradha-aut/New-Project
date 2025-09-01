2d3bf874dc17d44433df1e51002e2ac23942fc01
anuradha <anuradha.singh@aut.ac.nz>
2025-09-02 03:44:18 +1200

    First Commit

---
+++ src/teststudent/TestStudent.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teststudent;
+
+/**
+ *
+ * @author anusingh
+ */
+public class TestStudent {
+
+     private String name;
+
+    public TestStudent(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
---
+++ src/teststudent/TestStudentList.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package teststudent;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author anusingh
+ */
+public class TestStudentList {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TestStudent[] students = new TestStudent[2];
+        Scanner input = new Scanner(System.in);
+        for(int i = 0; i < students.length; i++){
+            System.out.println("Enter Student's name: ");
+            String name = input.nextLine();
+            TestStudent student = new TestStudent(name);
+            students[i] = student;            
+        }
+        
+        System.out.println("Printing the Students");
+        for (TestStudent st: students){
+            System.out.println("The student name is: "+ st.getName());
+        }
+        
+    }
+    
+}
